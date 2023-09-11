d4ae8afd2ad320c0a0ea5301dee886105a48f8b5
Jaewon Yun <woin2ee@gmail.com>
2023-09-11 23:56:12 +0900

    Update library type to dynamic

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "RxUtility",
+            type: .dynamic,
             targets: ["RxUtility"]),
     ],
     dependencies: [
